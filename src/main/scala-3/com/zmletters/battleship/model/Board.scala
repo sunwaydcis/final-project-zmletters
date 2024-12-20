ad5df84a1d6dbb2feb1fb5e47dd7729d9bc4de79
zmletters <zemingletters@gmail.com>
2024-12-20 10:40:39 +0800

    board add

--- src/main/scala-3/com/zmletters/battleship/model/Board.scala
+++ src/main/scala-3/com/zmletters/battleship/model/Board.scala
@@ -2,19 +2,33 @@ package com.zmletters.battleship.model
 
 class Board(val size: Int):
 
-  private val grid: Array[Array[Option[String]]] = Array.fill(size, size)(None)
+  private val grid: Array[Array[Option[Ship]]] = Array.fill(size, size)(None)
 
   def displayBoard(): Unit =
     for (row <- grid) {
       println(row.map{
-        case Some("S") => "S"
-        case Some("H") => "H"
-        case Some("M") => "M"
+        case Some(ship) => "S"
         case None => "-"
         case _ => "-"
       }.mkString(" "))
     }
 
+  def placeShip(ship: Ship, positions: List[(Int, Int)]): Boolean =
+    if (isPlacementValid(positions)) {
+      positions.foreach { case(x, y) =>
+        grid(x)(y) = Some(ship)
+      }
+      ship.place(positions)
+      true
+    } else {
+      false
+    }
+
+  private def isPlacementValid(positions: List[(Int, Int)]): Boolean =
+    positions.forall {
+      case (x, y) =>
+        x >= 0 && x < size && y >= 0 && y <= size && grid(x)(y).isEmpty
+    }
 
 object TestBoard extends App:
   val b1 = Board(10)
